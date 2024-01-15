e81a798cac280dc0fb202127c704e555d50b51d0
Cheikh Mbacké <mbackecheikhouna7@gmail.com>
2024-01-15 10:38:35 +0100

    vider le cacher après ajout/supression de crypto

--- backend/controllers/crypto/cryptoAPI.js
+++ backend/controllers/crypto/cryptoAPI.js
@@ -1,13 +1,12 @@
 const db = require("../../models");
 const CryptoSelection = db.CryptoSelection;
 const User = db.User;
-const axios = require('axios');
+const axios = require("axios");
 
-const NodeCache = require('node-cache');
+const NodeCache = require("node-cache");
 const myCache = new NodeCache({ stdTTL: 100, checkperiod: 120 });
 
 const getAdminCryptoList = async (req, res) => {
-
   const key = `cryptoList_default_admin`;
   const cachedData = myCache.get(key);
 
@@ -20,7 +19,7 @@ const getAdminCryptoList = async (req, res) => {
       "https://api.coingecko.com/api/v3/coins/markets",
       {
         params: {
-          vs_currency: 'eur',
+          vs_currency: "eur",
           order: "market_cap_desc",
           per_page: 100,
           page: 1,
@@ -41,7 +40,7 @@ const getAdminCryptoList = async (req, res) => {
 
 const getCryptoList = async (req, res) => {
   const { userId } = req;
-  let userCurrency = "eur"; 
+  let userCurrency = "eur";
 
   if (userId) {
     try {
@@ -54,7 +53,7 @@ const getCryptoList = async (req, res) => {
         userCurrency = user.currency;
       }
     } catch (error) {
-        throw error;
+      throw error;
     }
   }
 
@@ -97,41 +96,45 @@ const getCryptoList = async (req, res) => {
     res.status(200).json(response.data);
   } catch (error) {
     console.error("Erreur lors de la récupération des données crypto", error);
+    res.status(500).json({
+      message: "Erreur lors de la récupération de la liste des crypto-monnaies",
+    });
+  }
+};
+
+const getCryptoDetails = async (req, res) => {
+  // Utiliser req.params.cmid au lieu de req.params.id
+  const cryptoId = req.params.id;
+
+  // Vérifier si cryptoId est fourni
+  if (!cryptoId) {
+    return res
+      .status(400)
+      .json({ message: "Identifiant de crypto-monnaie manquant" });
+  }
+
+  const key = `cryptoDetails-${cryptoId}`;
+  const cachedData = myCache.get(key);
+
+  if (cachedData) {
+    return res.status(200).json(cachedData);
+  }
+
+  try {
+    const response = await axios.get(
+      `https://api.coingecko.com/api/v3/coins/${cryptoId}`
+    );
+    myCache.set(key, response.data, 3600); // Mise en cache pour 1 heure
+    res.status(200).json(response.data);
+  } catch (error) {
     res
       .status(500)
       .json({
         message:
-          "Erreur lors de la récupération de la liste des crypto-monnaies",
+          "Erreur lors de la récupération des détails de la crypto-monnaie",
       });
   }
 };
-
-
-const getCryptoDetails = async (req, res) => {
-    // Utiliser req.params.cmid au lieu de req.params.id
-    const cryptoId = req.params.id;
-
-    // Vérifier si cryptoId est fourni
-    if (!cryptoId) {
-        return res.status(400).json({ message: 'Identifiant de crypto-monnaie manquant' });
-    }
-
-    const key = `cryptoDetails-${cryptoId}`;
-    const cachedData = myCache.get(key);
-
-    if (cachedData) {
-        return res.status(200).json(cachedData);
-    }
-
-    try {
-        const response = await axios.get(`https://api.coingecko.com/api/v3/coins/${cryptoId}`);
-        myCache.set(key, response.data, 3600); // Mise en cache pour 1 heure
-        res.status(200).json(response.data);
-    } catch (error) {
-        res.status(500).json({ message: 'Erreur lors de la récupération des détails de la crypto-monnaie' });
-    }
-};
-
 
 const getCryptoHistory = async (req, res) => {
   const { userId } = req;
@@ -182,77 +185,86 @@ const getCryptoHistory = async (req, res) => {
     myCache.set(key, response.data, 86400); // Mise en cache pour 1 jour
     res.status(200).json(response.data);
   } catch (error) {
-    res
-      .status(500)
-      .send({
-        message:
-          error.message ||
-          "Erreur lors de la récupération de l'historique des prix",
-      });
+    res.status(500).send({
+      message:
+        error.message ||
+        "Erreur lors de la récupération de l'historique des prix",
+    });
   }
 };
 
-
-
 const addCrypto = async (req, res) => {
-    // Extraire les informations de la requête
-    const { cryptoId, nom } = req.body;
+  // Extraire les informations de la requête
+  const { cryptoId, nom } = req.body;
 
-    // Vérifier si les données nécessaires sont présentes
-    if (!cryptoId || !nom) {
-        return res.status(400).json({ message: "Les informations 'cryptoId' et 'nom' sont requises" });
+  // Vérifier si les données nécessaires sont présentes
+  if (!cryptoId || !nom) {
+    return res
+      .status(400)
+      .json({ message: "Les informations 'cryptoId' et 'nom' sont requises" });
+  }
+
+  try {
+    // Vérifier si la crypto-monnaie existe déjà dans la base de données
+    const existingCrypto = await CryptoSelection.findOne({
+      where: { cryptoId: cryptoId },
+    });
+    if (existingCrypto) {
+      return res
+        .status(409)
+        .json({ message: "Cette crypto-monnaie existe déjà" });
     }
 
-    try {
-        // Vérifier si la crypto-monnaie existe déjà dans la base de données
-        const existingCrypto = await CryptoSelection.findOne({ where: { cryptoId: cryptoId } });
-        if (existingCrypto) {
-            return res.status(409).json({ message: "Cette crypto-monnaie existe déjà" });
-        }
+    // Ajouter la nouvelle crypto-monnaie à la base de données
+    const newCrypto = await CryptoSelection.create({ cryptoId, nom });
 
-        // Ajouter la nouvelle crypto-monnaie à la base de données
-        const newCrypto = await CryptoSelection.create({ cryptoId, nom });
-
-        // Retourner la réponse
-        res.status(201).json({
-            message: "Crypto-monnaie ajoutée avec succès",
-            data: newCrypto
-        });
-    } catch (error) {
-        // Gérer les erreurs éventuelles
-        res.status(500).json({ message: "Erreur lors de l'ajout de la crypto-monnaie"});
-    }
+    myCache.flushAll();
+    // Retourner la réponse
+    res.status(201).json({
+      message: "Crypto-monnaie ajoutée avec succès",
+      data: newCrypto,
+    });
+  } catch (error) {
+    // Gérer les erreurs éventuelles
+    res
+      .status(500)
+      .json({ message: "Erreur lors de l'ajout de la crypto-monnaie" });
+  }
 };
 
-
 const deleteCrypto = async (req, res) => {
-    const cryptoId = req.params.id;
+  const cryptoId = req.params.id;
 
-    try {
-        // Rechercher la crypto-monnaie dans la base de données
-        const crypto = await CryptoSelection.findOne({ where: { cryptoId: cryptoId } });
-        
-        // Vérifier si la crypto-monnaie existe
-        if (!crypto) {
-            return res.status(404).json({ message: "Crypto-monnaie non trouvée" });
-        }
+  try {
+    // Rechercher la crypto-monnaie dans la base de données
+    const crypto = await CryptoSelection.findOne({
+      where: { cryptoId: cryptoId },
+    });
 
-        // Supprimer la crypto-monnaie de la base de données
-        await CryptoSelection.destroy({ where: { cryptoId: cryptoId } });
-
-        // Retourner la réponse
-        res.status(200).json({ message: "Crypto-monnaie supprimée avec succès" });
-    } catch (error) {
-        // Gérer les erreurs éventuelles
-        res.status(500).json({ message: "Erreur lors de la suppression de la crypto-monnaie" });
+    // Vérifier si la crypto-monnaie existe
+    if (!crypto) {
+      return res.status(404).json({ message: "Crypto-monnaie non trouvée" });
     }
+
+    // Supprimer la crypto-monnaie de la base de données
+    await CryptoSelection.destroy({ where: { cryptoId: cryptoId } });
+    
+    myCache.flushAll();
+    // Retourner la réponse
+    res.status(200).json({ message: "Crypto-monnaie supprimée avec succès" });
+  } catch (error) {
+    // Gérer les erreurs éventuelles
+    res
+      .status(500)
+      .json({ message: "Erreur lors de la suppression de la crypto-monnaie" });
+  }
 };
 
 module.exports = {
   getAdminCryptoList,
-    getCryptoList, 
-    getCryptoDetails,
-    getCryptoHistory,
-    addCrypto,
-    deleteCrypto
+  getCryptoList,
+  getCryptoDetails,
+  getCryptoHistory,
+  addCrypto,
+  deleteCrypto,
 };
